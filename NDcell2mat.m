e965774e980446ab3ce305ddb3e9d28793859dbc
Dominik Mattioli <35341510+domattioli@users.noreply.github.com>
2024-01-22 16:20:45 -0600

    Update NDcell2mat.m comments
    Comments contained references to depracated function name

--- NDcell2mat.m
+++ NDcell2mat.m
@@ -1,6 +1,6 @@
 function M = NDcell2mat(C,FILLER)
     %NDCELL2MAT Cell2mat for an array-cell structure of any length.
-    %   M = MYCELL2MAT(C) converts a unidimensional cell array CELL with
+    %   M = NDCELL2MAT(C) converts a unidimensional cell array CELL with
     %   contents of the same data type into a single matrix M. There is no
     %   need for the dimensions of the cell's contents to match, however C
     %   must be of dimension Nx1 or 1xN and all cell contents are expected
@@ -11,11 +11,11 @@ function M = NDcell2mat(C,FILLER)
     %   all cell contents of C. Given that not all contents of C may be the
     %   same length, any "extra" space will be replaced with a NaN in M.
     %
-    %   MYCELL2MAT is not supported for cell arrays containing objects.
+    %   NDCELL2MAT is not supported for cell arrays containing objects.
     %
     %   Example:
     %   C = {[0]; [0 1]; [0 1 2]; [0 1 2 3]; [4 0]};
-    %   M = MYCELL2MAT(C)
+    %   M = NDCELL2MAT(C)
     %   %returns
     %   M = [0 NaN NaN NaN; 0 1 NaN NaN; 0 1 2 NaN; 0 1 2 3; 4 0 NaN NaN]
     %
@@ -52,24 +52,24 @@ function M = NDcell2mat(C,FILLER)
     end
     
     % Step through cell contents via their lengths; extract entries to M.
-    Lmax= max(sC(:,1));                        	% Cell contents' max length.
+    Lmax = max(sC(:,1));                        % Cell contents' max length.
     if strcmp('nan',defaultFILLER)              % Fill with NaNs.
-        M	= NaN(length(C),Lmax);
+        M = NaN(length(C),Lmax);
     else                                        % Fill with zeros.
-        M	= zeros(length(C),Lmax);
+        M = zeros(length(C),Lmax);
     end
-    for L	= 1:Lmax
+    for L = 1:Lmax
         % Insert a matrix of the cells of length L into indexed rows of M.
-        iM	= sC(:,1) == L;                   	% Index cells of length L.
+        iM = sC(:,1) == L;                   	% Index cells of length L.
         try
-            M(iM,1:L)	= cell2mat(C(iM));
+            M(iM,1:L) = cell2mat(C(iM));
         catch   % Input cells might be row vectors.
-            M(iM,1:L)	= reshape(cell2mat(C(iM)),L,sum(iM))';
+            M(iM,1:L) = transpose(reshape(cell2mat(C(iM)),L,sum(iM)));
         end
     end
     
     % Transpose output M to reflect dimensionality of C, if needed.
     if transpose_M
-        M   = transpose(M);
+        M = transpose(M);
     end
 end
