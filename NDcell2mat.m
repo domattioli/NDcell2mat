991d29d53ade50ac6aca48904013120060da7572
Dominik Mattioli <35341510+domattioli@users.noreply.github.com>
2024-01-23 10:39:34 -0600

    Update NDcell2mat.m from old version

--- NDcell2mat.m
+++ NDcell2mat.m
@@ -1,75 +1,96 @@
-function M = NDcell2mat(C,FILLER)
-    %NDCELL2MAT Cell2mat for an array-cell structure of any length.
-    %   M = NDCELL2MAT(C) converts a unidimensional cell array CELL with
-    %   contents of the same data type into a single matrix M. There is no
-    %   need for the dimensions of the cell's contents to match, however C
-    %   must be of dimension Nx1 or 1xN and all cell contents are expected
-    %   to be vectors.
-    %
-    %   The dimensionality of M will be NxL or LxN, depending on the
-    %   dimensionality of C, where L corresponds to the maximum length of
-    %   all cell contents of C. Given that not all contents of C may be the
-    %   same length, any "extra" space will be replaced with a NaN in M.
-    %
-    %   NDCELL2MAT is not supported for cell arrays containing objects.
-    %
-    %   Example:
-    %   C = {[0]; [0 1]; [0 1 2]; [0 1 2 3]; [4 0]};
-    %   M = NDCELL2MAT(C)
-    %   %returns
-    %   M = [0 NaN NaN NaN; 0 1 NaN NaN; 0 1 2 NaN; 0 1 2 3; 4 0 NaN NaN]
-    %
-    %   See also cell2mat.
-    %======================================================================
-    
-    % Check I/O argument(s).
-    defaultFILLER    = 'nan';
-    if nargin == 2
-        nanStrings  = {'nan','nans','n','ns'};
-        zeroStrings = {'zero','zeros','z','zs'};
-        acceptableInput2    = [nanStrings;zeroStrings];
-        checkFILLER = find(sum(strcmpi(acceptableInput2,FILLER),2) == 1);
-        if any(checkFILLER)
-            defaultFILLER	= acceptableInput2{checkFILLER,1};
+function M = NDcell2Mat( C, V )
+%NDCELL2MAT Cell2mat for an array-cell structure of any length.
+%   M = NDCELL2MAT( C ) converts a unidimensional cell array C with
+%   contents of the same data type into a single matrix M. There is no
+%   need for the dimensions of the cell's contents to match.
+%
+%   M = NDCELL2MAT( C, V ) fills in incongruent dimensions specified by V.
+%   V must be a numeric value that specified the values that fill in the
+%   output M where values did not expist in that dimension of input C.
+%   Acceptable inputs for V are: nan, 0, or 1.
+%
+%   NDCELL2MAT currently only supports numeric data.
+%   
+%   % Example:  5x1
+%   C = { [0]; [1 2 3]; [4; 5]; NaN; [6 7 8 9 10] };
+%   M = NDCELL2MAT( C )
+%
+%   % Example:  3x2x1
+%   C = { 0, [1,2,3]; [4;5], NaN; [6,7,8,9], Inf };
+%   M = NDCELL2MAT( C, inf )
+%
+%   % Example:  Random cell input.
+%   C = cell( randi( 100, 1, 2 ) );
+%   for idx = 1:numel( C )
+%       C{ idx } = randi( 100, randi( 100, 1 ) );
+%   end
+%   M = NDCELL2MAT( C, eps );
+%   disp( size( M ) );
+%   disp( size( C ) );
+%   disp( max( cellfun( @numel, C( : ) ) ) );
+%
+%   % Example:  Practical application via graph vertices' degree.
+%   A = [0 10 20 30; 10 0 2 0; 20 2 0 1; 30 0 1 0];
+%   G = graph( A );
+%   C = cell( G.numnodes(), 1 );
+%   for idx = 1:G.numnodes()
+%       C{ idx } = G.neighbors( idx );
+%   end
+%   M   = NDCELL2MAT( C, randi( 100, 1 ) ) % View all nodes' neighbors at once.
+%
+%
+%   Written by: Dominik Mattioli
+%   Functionality revisions suggested by: Stephen Cobeldick
+%   (https://www.mathworks.com/matlabcentral/profile/authors/3102170).
+%
+%   See also cell2mat.
+%==========================================================================
+
+% Check input.
+narginchk( 1, 2 );
+nargoutchk( 0, 1 );
+if nargin == 2
+    if isstring( V ) || ischar( V )
+        warning( ['Inputted filler value ''%s'' detected as string when numeric was expected; ',...
+            'output may not be as expected/error may result.'], V );
+        if strcmpi( V, 'NaN' ) || strcmpi( V, 'NaNs' )
+            V   = NaN;
+        elseif strcmpi( V, 'Zero' ) || strcmpi( V, 'Zeros' ) || strcmpi( V, '0' )
+            V   = 0;
+        elseif strcmpi( V, 'Inf' ) || strcmpi( V, 'Infinity' )
+            V   = Inf;
+        elseif numel( str2num( V ) ) == 1 %#ok<*ST2NM>
+            V   = str2num( V );
+        else
+            V   = Inf;
         end
-    end
-    
-    % Determine largest length of cell contents.
-    sC  = cell2mat(cellfun(@(x) size(x),C,'UniformOutput',0));
-    
-    % Check input.
-    if sum(size(C) == 1) == 0 || all(sC(:,1) == sC(1,1))
-        if sC(1,1) > sC(1,2)                    % Make sure they don't concactenate.
-            C = cellfun(@transpose,C,'UniformOutput',0);
-        end
-        M	= cell2mat(C);                   	% Built-in cell2mat.
-        return                                 
-    elseif size(C,1) == 1
-        C   = transpose(C);                     % Accomodate for 1xN input.
-        transpose_M	= true;                     % Output will be LxN.
     else
-        transpose_M	= false;                    % Output will be NxN.
-    end
-    
-    % Step through cell contents via their lengths; extract entries to M.
-    Lmax = max(sC(:,1));                        % Cell contents' max length.
-    if strcmp('nan',defaultFILLER)              % Fill with NaNs.
-        M = NaN(length(C),Lmax);
-    else                                        % Fill with zeros.
-        M = zeros(length(C),Lmax);
-    end
-    for L = 1:Lmax
-        % Insert a matrix of the cells of length L into indexed rows of M.
-        iM = sC(:,1) == L;                   	% Index cells of length L.
-        try
-            M(iM,1:L) = cell2mat(C(iM));
-        catch   % Input cells might be row vectors.
-            M(iM,1:L) = transpose(reshape(cell2mat(C(iM)),L,sum(iM)));
+        if ~isnumeric( V )
+            V   = Inf;
         end
-    end
-    
-    % Transpose output M to reflect dimensionality of C, if needed.
-    if transpose_M
-        M = transpose(M);
     end
 end
+assert(all(cell2mat(cellfun(@isnumeric, C(:), 'UniformOutput', false))), ...
+    ['Inputted cell array contains elements that are non-numeric.',...
+    'Check that C does not contain nested cells or non-numeric data.'] );
+
+
+% Pre-assign output.
+S	= horzcat( size( C ), 1 );
+X	= find( S == 1, 1, 'first' );
+N	= max( cellfun( @numel, C( : ) ) );
+S( X )	= N;
+if nargin < 2
+    M	= NaN( S );
+else
+    M	= zeros( S ) + V;
+end
+
+% Walk across cells, converting to numeric values.
+Y	= num2cell( S );
+for k = 1:numel( C )
+    [Y{:}]  = ind2sub( size( C ), k );
+    Y{ X }	= 1:numel( C{ k } );
+    M( Y{ : } ) = C{ k }( : );
+end
+
